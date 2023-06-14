a833e9cfcad8094408e6d810cb1108a5f9811475
EdwingStivenMejiaSaenzCampus <edwingstiven20@gmail.com>
2023-06-14 11:23:32 -0500

    first commit

---
+++ e-commerce/controllers/controller.php
@@ -0,0 +1,10 @@
+<?php
+
+class Template{
+
+    public function index(){
+        include "views/template.php";
+    }
+}
+
+?>
---
+++ e-commerce/index.php
@@ -0,0 +1,9 @@
+
+<?php
+require_once "controllers/controller.php";
+
+$index = new Template();
+
+$index -> index();
+
+?>
